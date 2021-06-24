02ace60861234919ea6636a41d1fdc8c1f01ba52
ihch <nemu0sou@gmail.com>
2021-06-24 22:53:17 +0900

    いったんこみっと

--- main.ts
+++ main.ts
@@ -1,40 +1,53 @@
+let i = 0;
+const generateID = (): string => {
+  return Date().toString() + i++;
+}
+
 type Executor<T> = (
   resolve: (result: T) => void,
   reject: (error: unknown) => void
 ) => void;
 
 export class MyPromise<T> {
+  private id: string;
   private state: "pending" | "fulfilled" | "rejected" = "pending";
   private value!: T;
   private error!: unknown;
   private onResolved?: (res: T) => void;
   private onRejected?: (err: unknown) => void;
 
-  constructor(f: Executor<T>) {
+  constructor(i: number, f: Executor<T>) {
+    this.id = i.toString();
     f(
       (result) => {
         this.value = result;
         this.state = "fulfilled";
-        if (this.onResolved) {
-          this.onResolved(result);
-        }
+        this.callback();
         return this;
       },
       (error) => {
         this.error = error;
         this.state = "rejected";
-        if (this.onRejected) {
-          this.onRejected(error);
-        }
+        this.callback();
         return this;
       }
     );
   }
 
-  then<U>(g: (result: T) => MyPromise<U> | U): MyPromise<U> {
+  callback() {
+    if (this.state === 'fulfilled' && this.onResolved) {
+      this.onResolved(this.value);
+    }
+    if (this.state === 'rejected' && this.onRejected) {
+      this.onRejected(this.error);
+    }
+  }
+
+  then<U>(i: number, g: (result: T) => MyPromise<U> | U): MyPromise<U> {
     const t = this;
-    return new MyPromise(function (resolve, reject) {
+    return new MyPromise(i, function (resolve, reject) {
       t.onResolved = (res) => {
+        console.log(t.id, i);
         try {
           const r = g(res);
           if (r instanceof MyPromise) {
@@ -50,10 +63,11 @@ export class MyPromise<T> {
     });
   }
 
-  catch<U>(g: (error: unknown) => MyPromise<U> | U): MyPromise<U> {
+  catch<U>(i: number, g: (error: unknown) => MyPromise<U> | U): MyPromise<U> {
     const t = this;
-    return new MyPromise(function (resolve, reject) {
+    return new MyPromise(i, function (resolve, reject) {
       t.onRejected = (res) => {
+        console.log(t.id, i);
         try {
           const e = g(res);
           if (e instanceof MyPromise) {
@@ -71,25 +85,29 @@ export class MyPromise<T> {
 }
 
 function main() {
-  new MyPromise((resolve) => {
+  new MyPromise(0, (resolve) => {
     setTimeout(() => {
       console.log("hello");
       resolve(1);
     }, 3000);
   })
-    .then((v) => {
+    .then(1, (v) => {
       console.log("hello world then1:", v);
       return 2;
     })
-    .then((v) =>
-      setTimeout(() => {
-        console.log("then2:", v);
-      }, 1000)
+    .then(2, (v) =>
+      {
+        setTimeout(() => {
+          console.log("then2:", v);
+        }, 1000);
+        return 3;
+      }
     )
-    .then(() => {
+    .then(3, (v) => console.log('asdf', v))
+    .then(4, () => {
       throw new Error("kowareta-");
     })
-    .catch((e) => console.log(e));
+    .catch(5, (e) => console.log(e));
 }
 
 main();
